56e1cbd0e943b6f380d8bd1d2f938bc4c662a867
Nicholas Michel <nickmichel100@gmail.com>
2016-11-22 16:25:50 -0500

    trying to finish parallax

--- EricsBees/app/controllers/bees_controller.rb
+++ EricsBees/app/controllers/bees_controller.rb
@@ -1,2 +1,16 @@
 class BeesController < ApplicationController
+  def honey
+  end
+
+  def services
+  end
+
+  def press
+  end
+
+  def contact
+  end
+
+  def index
+  end
 end
--- EricsBees/config/routes.rb
+++ EricsBees/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bees
+  # resources :bees
   root to: "bees#index"
+  # get '/honey' => 'bees#honey'
+  (BeesController.action_methods).each do |route_sym|
+  match route_sym.to_s, to: "bees##{route_sym.to_s}", via: 'get'
+  end
+
 end
